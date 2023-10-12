7b303538ecb24c8fffd1c4f257f0f67c4f46bffb
Geobellz22 <st377126@gmail.com>
2023-10-12 16:32:37 +0000

    Task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,9 @@
 --with id, email and name
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
     name VARCHAR(255)
-);
+    PRIMARY KEY(id),
+    UNIQUE(email),
+) ;
