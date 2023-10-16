6ab29031c11254b7c30a4f2ffb5aba6cb0b4a682
Geobellz22 <st377126@gmail.com>
2023-10-16 21:24:29 +0000

    Task 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,16 @@
+-- script that create trigger 
+-- decrease the quantity of an item
+-- after adding a new order
+
+DELIMITER $$
+
+CREATE TRIGGER decrease_qunatity
+AFTER INSERTING ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity - NEW_number
+    WHERE name = New.item_name;
+END $$
+
+DELIMTER $$
