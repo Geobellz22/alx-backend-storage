7a6929b2e29afdcce7d74f97f3c3c5316b20bd4c
Geobellz22 <st377126@gmail.com>
2023-10-16 12:45:06 +0000

    Task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,8 +2,8 @@
 --with id, email and name
 --Drop the tables if it exists
 
-CREATE TABLE IF NOT EXISTS `users` (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     email CHAR(255) NOT NULL UNIQUE,
-    name CHAR(255)
+    name CHAR(255),
 );
