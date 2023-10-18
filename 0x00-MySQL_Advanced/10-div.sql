dabc27e92f831b430ef6afd55c1c92b9fb99c5c7
Geobellz22 <st377126@gmail.com>
2023-10-18 23:00:43 +0000

    Task 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,16 +1,18 @@
 -- create a function safeDiv that divides and 
 -- returns the first by the second number
 
+
 DELIMITER $$ ;
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
 DETERMINISTIC
 BEGIN
     DECLARE result FLOAT;
-        IF b = 0 THEN;
-           RETURN 0;
-        ELSE
-            RETURN a / b;
-        END IF;
-    END $$
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+
+    SET result = a / b;
+    RETURN result;
+END$$
 DELIMITER ; $$
