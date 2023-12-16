1b8ee5a88a535fda468134a47a7436f6e22bfe29
Geobellz22 <st377126@gmail.com>
2023-12-16 22:49:05 +0000

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,3 +16,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int]:
+        """initialize get method"""
+        data = self._redis.get(key)
+        if data is None:
+            return data
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        """initialize get_str method"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """get_int method"""
+        return self.get(key, fn=int)
